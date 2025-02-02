cdb1bd15d3a0b14910d28ab8a4e06a2bacb0b7e0
harshitmahal <harshit.mahal@gmail.com>
2025-02-02 19:38:21 +0530

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 // import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState()
+
+  useEffect(() => {
+    const userData = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        console.log(data);
+        setUsers(data);
+
+      } catch (e) { console.log(e) }
+    }
+    userData();
+
+  }, [])
+
+
+
   return (
     <div className="App">
       Hello World
